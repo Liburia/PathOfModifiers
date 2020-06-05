726ad87ab946b26541a944e61226979954ee0a6e
Kono <CuHyC2009@gmail.com>
2020-06-05 23:30:32 +0500

    Added HelmetReflectNova suffix.

---
+++ Affixes/Items/Suffixes/HelmetReflectNova.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+using System;
+using System.Linq;
+using Terraria;
+using Terraria.Utilities;
+using System.IO;
+using System.Collections.Generic;
+using Terraria.ModLoader.IO;
+using PathOfModifiers.Projectiles;
+using Terraria.ID;
+using PathOfModifiers.ModNet.PacketHandlers;
+using Terraria.DataStructures;
+
+namespace PathOfModifiers.Affixes.Items.Suffixes
+{
+    public class HelmetReflectNova : AffixTiered<TTFloat, TTFloat, TTFloat>, ISuffix
+    {
+        public override double Weight { get; } = 1;
+
+        public override TTFloat Type1 { get; } = new TTFloat()
+        {
+            TwoWay = false,
+            IsRange = true,
+            Tiers = new TTFloat.WeightedTier[]
+            {
+                new TTFloat.WeightedTier(0f, 3),
+                new TTFloat.WeightedTier(0.5f, 2.5),
+                new TTFloat.WeightedTier(1f, 2),
+                new TTFloat.WeightedTier(1.5f, 1.5),
+                new TTFloat.WeightedTier(2f, 1),
+                new TTFloat.WeightedTier(2.5f, 0.5),
+                new TTFloat.WeightedTier(3f, 0),
+            },
+        };
+        public override TTFloat Type2 { get; } = new TTFloat()
+        {
+            TwoWay = false,
+            IsRange = true,
+            Tiers = new TTFloat.WeightedTier[]
+            {
+                new TTFloat.WeightedTier(0f, 3),
+                new TTFloat.WeightedTier(0.005f, 2.5),
+                new TTFloat.WeightedTier(0.01f, 2),
+                new TTFloat.WeightedTier(0.015f, 1.5),
+                new TTFloat.WeightedTier(0.02f, 1),
+                new TTFloat.WeightedTier(0.025f, 0.5),
+                new TTFloat.WeightedTier(0.03f, 0),
+            },
+        };
+        public override TTFloat Type3 { get; } = new TTFloat()
+        {
+            TwoWay = false,
+            IsRange = true,
+            Tiers = new TTFloat.WeightedTier[]
+            {
+                new TTFloat.WeightedTier(6f, 3),
+                new TTFloat.WeightedTier(5f, 2.5),
+                new TTFloat.WeightedTier(4f, 2),
+                new TTFloat.WeightedTier(3f, 1.5),
+                new TTFloat.WeightedTier(2f, 1),
+                new TTFloat.WeightedTier(1f, 0.5),
+                new TTFloat.WeightedTier(0f, 0),
+            },
+        };
+        public override WeightedTierName[] TierNames { get; } = new WeightedTierName[] {
+            new WeightedTierName("of Bristles", 0.5),
+            new WeightedTierName("of Barbs", 1),
+            new WeightedTierName("or Thorns", 1.5),
+            new WeightedTierName("of Brambles", 2),
+            new WeightedTierName("of Spikes", 2.5),
+            new WeightedTierName("of Reflect", 3),
+        };
+
+        public uint lastProcTime = 0;
+
+        public override bool CanBeRolled(AffixItemItem pomItem, Item item)
+        {
+            return
+                AffixItemItem.IsHeadArmor(item);
+        }
+
+        public override string GetTolltipText(Item item)
+        {
+            return $"Nova for { Type1.GetValueFormat() }% of damage taken + { Type2.GetValueFormat() }% of target's HP as damage when hit ({ Type3.GetValueFormat(1) }s CD)";
+        }
+
+        public override void OnHitByNPC(Item item, Player player, NPC npc, int damage, bool crit)
+        {
+            SpawnNova(item, player, damage);
+        }
+        public override void OnHitByProjectile(Item item, Player player, Projectile projectile, int damage, bool crit)
+        {
+            SpawnNova(item, player, damage);
+        }
+        public override void OnHitByPvp(Item item, Player player, Player attacker, int damage, bool crit)
+        {
+            SpawnNova(item, player, damage);
+        }
+
+        void SpawnNova(Item item, Player player, int damageTaken)
+        {
+            if (AffixItemItem.IsArmorEquipped(item, player) && (Main.GameUpdateCount - lastProcTime) >= (int)Math.Round(Type2.GetValue() * 60))
+            {
+                Projectile.NewProjectile(player.Center, Vector2.Zero, ModContent.ProjectileType<ReflectNova>(), (int)Math.Round(damageTaken * Type1.GetValue()), 0, player.whoAmI, Type2.GetValue());
+
+                lastProcTime = Main.GameUpdateCount;
+            }
+        }
+
+        public override Affix Clone()
+        {
+            var affix = (HelmetReflectNova)base.Clone();
+
+            affix.lastProcTime = lastProcTime;
+
+            return affix;
+        }
+    }
+}
---
+++ Projectiles/ReflectNova.cs
@@ -0,0 +1,135 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PathOfModifiers.Dusts;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PathOfModifiers.Projectiles
+{
+    public class ReflectNova : ModProjectile, INonTriggerringProjectile
+    {
+        public static float startScale = 0.2f;
+        public static float maxScale = 3;
+        public static float scaleIncrease = 0.3f;
+        public static Vector2 size = new Vector2(400, 400);
+        public static float collisionScale = 0.7f;
+
+        bool[] hitNPCs = new bool[Main.maxNPCs];
+        bool hitPlayer;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Thorn Nova");
+            Main.projFrames[projectile.type] = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.scale = startScale;
+            projectile.Size = size * collisionScale * startScale;
+            projectile.timeLeft = 600;
+            projectile.friendly = true;
+            projectile.tileCollide = false;
+            projectile.ignoreWater = true;
+        }
+
+        public override void AI()
+        {
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            int frameHeight = texture.Height / Main.projFrames[projectile.type];
+            if (projectile.scale < maxScale)
+            {
+                projectile.scale += scaleIncrease;
+                if (projectile.scale >= maxScale)
+                    projectile.scale = maxScale;
+                projectile.Size = size * collisionScale * projectile.scale;
+                projectile.position -= new Vector2(texture.Width, frameHeight) * collisionScale * scaleIncrease / 2;
+            }
+            else
+            {
+                projectile.Kill();
+            }
+
+
+            projectile.frameCounter++;
+            if (projectile.frameCounter >= 5)
+            {
+                projectile.frameCounter = 0;
+                projectile.frame++;
+                if (projectile.frame >= 3)
+                {
+                    projectile.frame = 0;
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+                Dust.NewDust(projectile.position, projectile.width, projectile.height, ModContent.DustType<ReflectNovaDebris>());
+
+
+            if (Main.netMode != NetmodeID.Server)
+            {
+                Player owner = Main.player[projectile.owner];
+
+                Player player = Main.LocalPlayer;
+                if (!hitPlayer && PoMUtil.CanHitPvp(owner, player))
+                {
+                    Rectangle localRect = player.getRect();
+                    if (localRect.Intersects(projectile.Hitbox))
+                    {
+                        player.Hurt(PlayerDeathReason.ByPlayer(projectile.owner), projectile.damage + (int)Math.Round(player.statLife * projectile.ai[0]), player.direction, true);
+                        hitPlayer = true;
+                    }
+                }
+
+                if (Main.myPlayer == projectile.owner)
+                {
+                    for (int i = 0; i < Main.maxNPCs; i++)
+                    {
+                        NPC npc = Main.npc[i];
+                        if (!hitNPCs[i] && PoMUtil.CanHitNPC(npc))
+                        {
+                            Rectangle npcRect = npc.getRect();
+                            if (npcRect.Intersects(projectile.Hitbox))
+                            {
+                                owner.ApplyDamageToNPC(npc, projectile.damage + (int)Math.Round(npc.lifeMax * projectile.ai[0]), 1, npc.direction, false);
+                                hitNPCs[i] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+        {
+            Texture2D texture = Main.projectileTexture[projectile.type];
+            int frameHeight = texture.Height / Main.projFrames[projectile.type];
+            int startY = frameHeight * projectile.frame;
+            Rectangle sourceRectangle = new Rectangle(0, startY, texture.Width, frameHeight);
+            Vector2 origin = sourceRectangle.Size() / 2f;
+
+            Color drawColor = projectile.GetAlpha(lightColor);
+            Main.spriteBatch.Draw(texture,
+                projectile.Center - Main.screenPosition + new Vector2(0f, projectile.gfxOffY),
+                sourceRectangle, drawColor, projectile.rotation, origin, projectile.scale, SpriteEffects.None, 0f);
+
+            return false;
+        }
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            return false;
+        }
+        public override bool CanHitPlayer(Player target)
+        {
+            return false;
+        }
+        public override bool CanHitPvp(Player target)
+        {
+            return false;
+        }
+    }
+}
