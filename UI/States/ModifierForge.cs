94d9e7260c7bc4d794a23fd3169c5210109e83a1
Kono <CuHyC2009@gmail.com>
2021-08-20 21:15:43 +0500

    Added text section to the forge UI

--- UI/States/ModifierForge.cs
+++ UI/States/ModifierForge.cs
@@ -13,6 +13,8 @@ using PathOfModifiers.UI.Elements;
 using Terraria.Localization;
 using Terraria.ModLoader;
 using PathOfModifiers.UI.States.ModifierForgeElements;
+using PathOfModifiers.UI.Chat;
+using Terraria.ID;
 
 namespace PathOfModifiers.UI.States
 {
@@ -105,6 +107,13 @@ namespace PathOfModifiers.UI.States
     class ModifierForge : UIState
 	{
         UIDraggablePanel panel;
+
+        UIText freeAffixes;
+        UIText freePrefixes;
+        UIText freeSuffixes;
+
+        UIText affixTextItemName;
+        UIList<AffixText> affixTextList;
 
         public override void OnInitialize()
         {
@@ -200,16 +209,33 @@ namespace PathOfModifiers.UI.States
                         actionParent.MinHeight.Set(0f, 1f);
                         actionSection.Append(actionParent);
                         {
-                            UIItemSlot item = new();
-                            item.MinWidth.Set(75f, 0f);
-                            item.MinHeight.Set(75f, 0f);
-                            actionParent.Append(item);
 
                             UIItemSlot fragment = new();
                             fragment.MinWidth.Set(75f, 0f);
-                            fragment.MinHeight.Set(75f, 0f);
-                            fragment.Left.Set(-fragment.MinWidth.Pixels, 1f);
+                            fragment.MinHeight.Set(fragment.MinWidth.Pixels, 0f);
+                            var fragmentTexture = ModContent.Request<Texture2D>($"PathOfModifiers/Items/ModifierFragment");
+                            fragment.CanInsertItem = (Item item) => item.type == ModContent.ItemType<Items.ModifierFragment>();
+                            fragment.OnDrawGhostItem += delegate (SpriteBatch sb, Vector2 position, float scale)
+                            {
+                                sb.Draw(fragmentTexture.Value, position, null, Color.White * 0.3f, 0f, fragmentTexture.Size() / 2f, scale, SpriteEffects.None, 0f);
+                            };
                             actionParent.Append(fragment);
+
+                            UIItemSlot item = new();
+                            item.MinWidth.Set(fragment.MinWidth.Pixels, 0f);
+                            item.MinHeight.Set(fragment.MinWidth.Pixels, 0f);
+                            item.Left.Set(-fragment.MinWidth.Pixels, 1f);
+                            var itemTexture = ModContent.Request<Texture2D>($"Terraria/Images/Item_{ ItemID.GoldBroadsword }");
+                            item.CanInsertItem = (Item item) => ItemItem.IsRollable(item);
+                            item.OnDrawGhostItem += delegate (SpriteBatch sb, Vector2 position, float scale)
+                            {
+                                sb.Draw(itemTexture.Value, position, null, Color.White * 0.3f, 0f, itemTexture.Size() / 2f, scale, SpriteEffects.None, 0f);
+                            };
+                            item.OnItemInserted += delegate (Item item)
+                            {
+                                UpdateItemText(item);
+                            };
+                            actionParent.Append(item);
 
                             UIButton reforge = new();
                             reforge.HAlign = 0.5f;
@@ -218,17 +244,140 @@ namespace PathOfModifiers.UI.States
                             reforge.MinWidth.Set(0f, 1f);
                             actionParent.Append(reforge);
                             {
-                                UIText text = new("Forge", 1f, true);
+                                UIText text = new("Forge", 0.9f, true);
                                 text.HAlign = 0.5f;
                                 text.VAlign = 0.5f;
                                 text.IgnoresMouseInteraction = true;
                                 reforge.Append(text);
                             }
+                        }
+                    }
 
+                    UIElement textSection = new();
+                    textSection.HAlign = 0.5f;
+                    textSection.Top.Set(0f, 0.5f);
+                    textSection.MinWidth.Set(0f, 0.9f);
+                    textSection.MinHeight.Set(0f, 0.5f);
+                    content.Append(textSection);
+                    {
+                        UIElement freeAffixSection = new();
+                        freeAffixSection.MinHeight.Set(0f, 1f);
+                        textSection.Append(freeAffixSection);
+                        {
+                            freeAffixes = new("[3/7]");
+                            freeAffixes.HAlign = 0.5f;
+                            freeAffixes.OnUpdate += delegate (UIElement affectedElement)
+                            {
+                                if (freeAffixes.IsMouseHovering)
+                                    Main.instance.MouseText("Total number of affixes on the item");
+                            };
+                            freeAffixSection.Append(freeAffixes);
+                            freeAffixSection.MinWidth.Set(freeAffixes.GetDimensions().Width, 0f);
+
+                            freePrefixes = new("[2/5]");
+                            freePrefixes.TextColor = Affix.prefixColor;
+                            freePrefixes.HAlign = 0.5f;
+                            freePrefixes.Top.Set(freeAffixes.MinHeight.Pixels + UICommon.spacing * 3, 0f);
+                            freePrefixes.OnUpdate += delegate (UIElement affectedElement)
+                            {
+                                if (freePrefixes.IsMouseHovering)
+                                    Main.instance.MouseText("Number of prefixes on the item");
+                            };
+                            freeAffixSection.Append(freePrefixes);
+
+                            freeSuffixes = new("[1/4]");
+                            freeSuffixes.TextColor = Affix.suffixColor;
+                            freeSuffixes.HAlign = 0.5f;
+                            freeSuffixes.Top.Set(freePrefixes.Top.Pixels + freePrefixes.MinHeight.Pixels + UICommon.spacing * 3, 0f);
+                            freeSuffixes.OnUpdate += delegate (UIElement affectedElement)
+                            {
+                                if (freeSuffixes.IsMouseHovering)
+                                    Main.instance.MouseText("Number of suffixes on the item");
+                            };
+                            freeAffixSection.Append(freeSuffixes);
+                        }
+
+                        UIElement itemText = new();
+                        itemText.Left.Set(freeAffixSection.MinWidth.Pixels + UICommon.spacing, 0f);
+                        itemText.MinWidth.Set(-freeAffixSection.MinWidth.Pixels, 1f);
+                        itemText.MinHeight.Set(0f, 1f);
+                        textSection.Append(itemText);
+                        {
+                            affixTextItemName = new("Rare Beads of Annahilatikon", 0.8f);
+                            affixTextItemName.TextColor = Color.Orange;
+                            itemText.Append(affixTextItemName);
+
+                            affixTextList = new();
+                            affixTextList.Top.Set(affixTextItemName.GetDimensions().Height + UICommon.spacing, 0f);
+                            affixTextList.MinWidth.Set(0f, 1f);
+                            affixTextList.MinHeight.Set(-affixTextItemName.GetDimensions().Height, 1f);
+                            affixTextList.ListPadding = 0f;
+                            itemText.Append(affixTextList);
+                            {
+                                for (int i = 0; i < 10; i++)
+                                {
+                                    AffixText affixText = new();
+                                    affixText.MaxWidth.Set(0f, 1f);
+                                    affixText.TextColor = Color.Green;
+                                    affixTextList.Add(affixText);
+                                    affixText.SetText($"{ Keyword.Bleed.GenerateTag() }gamepedia is soo bad on russian, literally 90% of in-game stuff just doesn't exists there as page, while fandom is 95% full", 0.7f);
+                                }
+                            }
                         }
                     }
                 }
 			}
+        }
+
+        void UpdateItemText(Item item)
+        {
+            foreach (var text in affixTextList)
+            {
+                text.SetText("");
+            }
+
+            if (item.TryGetGlobalItem<ItemItem>(out var modItem))
+            {
+                freeAffixes.SetText($"[{modItem.affixes.Count}/{modItem.rarity.maxAffixes}]");
+                freePrefixes.SetText($"[{modItem.prefixes.Count}/{modItem.rarity.maxPrefixes}]");
+                freeSuffixes.SetText($"[{modItem.suffixes.Count}/{modItem.rarity.maxSuffixes}]");
+
+                affixTextItemName.SetText(item.Name);
+
+                int i = 0;
+                for (; i < modItem.prefixes.Count; i++)
+                {
+                    if (i >= affixTextList.Count) break;
+
+                    var prefix = modItem.prefixes[i];
+                    var text = affixTextList._items[i];
+
+                    text.TextColor = prefix.Color;
+                    text.SetText(prefix.GetTolltipText());
+                }
+                int j = 0;
+                for (; j < modItem.suffixes.Count; j++)
+                {
+                    int textIndex = i + j;
+                    if (textIndex >= affixTextList.Count) break;
+
+                    var suffix = modItem.suffixes[j];
+                    var text = affixTextList._items[textIndex];
+
+                    text.TextColor = suffix.Color;
+                    text.SetText(suffix.GetTolltipText());
+                }
+
+                affixTextList.Recalculate();
+            }
+            else
+            {
+                freeAffixes.SetText("[0/0]");
+                freePrefixes.SetText("[0/0]");
+                freeSuffixes.SetText("[0/0]");
+
+                affixTextItemName.SetText("No item");
+            }
         }
     }
 }
---
+++ UI/States/ModifierForgeElements/AffixText.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using ReLogic.Graphics;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.GameContent.UI.Elements;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.UI;
+using Terraria.UI.Chat;
+
+namespace PathOfModifiers.UI.Elements
+{
+	public class AffixText : UIElement
+	{
+		private DynamicSpriteFont _font;
+		private object _text = "";
+		private float _textScale = 1f;
+		private Color _color = Color.White;
+		public bool DynamicallyScaleDownToWidth;
+		private TextSnippet[] _parsedText = new TextSnippet[0];
+
+		public string Text => _text.ToString();
+
+		public Color TextColor
+		{
+			get
+			{
+				return _color;
+			}
+			set
+			{
+				_color = value;
+			}
+		}
+
+		public AffixText()
+		{
+			_font = FontAssets.MouseText.Value;
+		}
+
+		public void SetText(string text)
+		{
+			_text = text;
+			Recalculate();
+		}
+		public void SetText(LocalizedText text)
+		{
+			_text = text;
+			Recalculate();
+		}
+		public void SetText(string text, float textScale)
+		{
+			_text = text;
+			_textScale = textScale;
+			Recalculate();
+		}
+		public void SetText(LocalizedText text, float textScale)
+		{
+			_text = text;
+			_textScale = textScale;
+			Recalculate();
+		}
+
+        public override void Recalculate()
+        {
+			InternalSetText(_text, _textScale);
+			base.Recalculate();
+        }
+
+        private void InternalSetText(object text, float textScale)
+		{
+			_text = text;
+			_textScale = textScale;
+			_parsedText = ChatManager.ParseMessage(Text, TextColor).ToArray();
+
+			var textSize = UICommon.Text.Measure(_parsedText, _font, _textScale, MaxWidth.GetValue(Parent.GetInnerDimensions().Width));
+
+			MinWidth.Set(textSize.X + PaddingLeft + PaddingBottom, 0f);
+			MinHeight.Set(textSize.Y + PaddingTop + PaddingBottom, 0f);
+		}
+
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			CalculatedStyle innerDimensions = GetInnerDimensions();
+			Vector2 pos = innerDimensions.Position();
+
+			//var size = UICommon.Text.Measure(_parsedText, _font, _textScale, innerDimensions.Width);
+			//spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle((int)pos.X, (int)pos.Y, (int)size.X, (int)size.Y), Color.Red);
+
+            int? hoveredSnippet = UICommon.Text.Draw(spriteBatch, _parsedText, _font, pos, _textScale, innerDimensions.Width);
+            if (hoveredSnippet.HasValue)
+            {
+                _parsedText[hoveredSnippet.Value].OnHover();
+                if (Main.mouseLeft && Main.mouseLeftRelease)
+                    _parsedText[hoveredSnippet.Value].OnClick();
+            }
+        }
+	}
+}
