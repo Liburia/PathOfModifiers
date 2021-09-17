fb23b7defe4fdf7d6aba23480bf23bc93121e5ff
Kono <CuHyC2009@gmail.com>
2021-09-17 17:16:06 +0500

    Fixed tool speed not working correctly

--- Affixes/Items/Prefixes/AccessoryToolSpeed.cs
+++ Affixes/Items/Prefixes/AccessoryToolSpeed.cs
@@ -23,12 +23,12 @@ namespace PathOfModifiers.Affixes.Items.Prefixes
             IsRange = true,
             Tiers = new TTFloat.WeightedTier[]
             {
-                new TTFloat.WeightedTier(-0.1f, -0.07f, 0.5),
-                new TTFloat.WeightedTier(-0.07f, -0.04f, 1),
-                new TTFloat.WeightedTier(-0.04f, 0.01f, 2),
-                new TTFloat.WeightedTier(0.01f, 0.04f, 2),
-                new TTFloat.WeightedTier(0.04f, 0.07f, 1),
-                new TTFloat.WeightedTier(0.07f, 0.1f, 0.5),
+                new TTFloat.WeightedTier(0.30f, 0.20f, 0.5),
+                new TTFloat.WeightedTier(0.20f, 0.10f, 1),
+                new TTFloat.WeightedTier(0.10f, 0.01f, 2),
+                new TTFloat.WeightedTier(-0.01f, -0.10f, 2),
+                new TTFloat.WeightedTier(-0.10f, -0.20f, 1),
+                new TTFloat.WeightedTier(-0.20f, -0.30f, 0.5),
             },
         };
         public override WeightedTierName[] TierNames { get; } = new WeightedTierName[] {
@@ -50,7 +50,7 @@ namespace PathOfModifiers.Affixes.Items.Prefixes
         public override string GetAffixText(bool useChatTags = false)
         {
             var valueRange1 = UI.Chat.ValueRangeTagHandler.GetTextOrTag(Type1.GetCurrentValueFormat(), Type1.GetMinValueFormat(), Type1.GetMaxValueFormat(), useChatTags);
-            char plusMinus = Type1.GetValue() < 0 ? '-' : '+';
+            char plusMinus = Type1.GetValue() < 0 ? '+' : '-';
             return $"{ plusMinus }{ valueRange1 }% pickaxe speed";
         }
 
