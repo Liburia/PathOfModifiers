dd19c15ff13aa80409f072deaf549a7d3e74884e
Liburia <CuHyC2009@gmail.com>
2023-08-08 23:33:30 +0500

    Fixed shock and chill not affecting damage

--- BuffNPC.cs
+++ BuffNPC.cs
@@ -185,19 +185,19 @@ namespace PathOfModifiers
 
         public override void ModifyHitByItem(NPC npc, Player player, Item item, ref NPC.HitModifiers modifiers)
         {
-            var damage = ShockModifyDamageTaken(modifiers.FinalDamage);
+            modifiers.FinalDamage = ShockModifyDamageTaken(modifiers.FinalDamage);
         }
         public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref NPC.HitModifiers modifiers)
         {
-            var damage = ShockModifyDamageTaken(modifiers.FinalDamage);
+            modifiers.FinalDamage = ShockModifyDamageTaken(modifiers.FinalDamage);
         }
         public override void ModifyHitPlayer(NPC npc, Player target, ref Player.HurtModifiers modifiers)
         {
-            var damage = ChillModifyDamageDealt(modifiers.FinalDamage);
+            modifiers.FinalDamage = ChillModifyDamageDealt(modifiers.FinalDamage);
         }
         public override void ModifyHitNPC(NPC npc, NPC target, ref NPC.HitModifiers modifiers)
         {
-            var damage = ChillModifyDamageDealt(modifiers.FinalDamage);
+            modifiers.FinalDamage = ChillModifyDamageDealt(modifiers.FinalDamage);
         }
     }
 }
